00f5c9d01d0d996bd2b744f2a505f7baec71529c
k-learn <pythonmk0@gmail.com>
2024-01-22 23:01:54 +0530

    Finish step 1 to step 4

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
-const express = require("express");
+import express from "express";
+import rootRouter from "./routes";
 
+const app = express();
 
+app.use("/api/v1", rootRouter);
